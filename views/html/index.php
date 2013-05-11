0eddd9771f4697fd23f218ebed1ffa3955709608
nikom2532 <nikom2532@hotmail.com>
2013-05-12 01:53:54 +0700

    Views: add education contents

--- views/html/index.php
+++ views/html/index.php
@@ -9,6 +9,19 @@
 </div>
 <div id="education">
 	<div class="myname">Education</div>
+	
+	
+	<div id="background-education" class="background-education edit-default"><h3>Education<span class="edit-tools"><a href="javascript:void(0);" class="edit-add" data-add-type="background-education" data-trk="{&quot;componentKey&quot; : &quot;profile-edit-education-form&quot;, &quot;infoParams&quot; : { &quot;source&quot; : &quot;prof-edit-background-add_education-link&quot;}}">Add education</a><span class="edit-order edit-section-order">Re-order section</span></span></h3><form name="sectionReorderForm" method="POST" action="/profile/section-reorder-submit"><input type="hidden" name="sectionType" value="EDU"><input type="hidden" name="sectionOrder" value="" id="sectionOrder-sectionReorderForm"><input type="hidden" name="timestamp" value="1368200410380" id="timestamp-sectionReorderForm"><input type="hidden" name="csrfToken" value="ajax:2813931629992919616" id="csrfToken-sectionReorderForm"><input type="hidden" name="sourceAlias" value="0_fvzmxERg2KDWVPMXj45hKnq3clxRTWA8ZwknG4OOUxF" id="sourceAlias-sectionReorderForm"></form><div id="education-blank-form" class="profile-blank-form"><!----></div><div id="education-blank" class="profile-blank"></div><div id="education-45113784" class="editable-item section-item"><div id="education-45113784-view"><div class="education  first"><h4 class="summary fn org"><a href="/college/?eduSchool=17282&amp;trk=prof-edu-school-name" title="Find other members who attended Sataban Technology Prajomglao Chaokuntaharn Ladkrabang">Sataban Technology Prajomglao Chaokuntaharn Ladkrabang</a><span class="edit-tools"><a href="javascript:void(0);" class="edit-section" data-trk="{&quot;componentKey&quot; : &quot;profile-edit-education-form&quot;, &quot;infoParams&quot; : { &quot;source&quot; : &quot;prof-edit-background-edit_education-link&quot;, &quot;educationID&quot; : &quot;45113784&quot;}}">Edit</a><span class="edit-order">Re-order</span></span></h4><h5><span class="degree">Bachelor of Science (B.Sc.), </span><span class="major"><a href="/search?search=&amp;keywords=Computer+Science+International+Program&amp;sortCriteria=R&amp;keepFacets=true&amp;trk=prof-edu-field_of_study" title="Find users with this keyword">Computer Science International Program</a></span></h5><span class="education-date"><time datetime="2008">2008</time><time datetime="2012"> – 2012</time></span><p class="notes">be a student at KMITL<br>
+in Computer Science International Programme</p><dl class="education-associated"><dt data-trk-code="prof-edu-project-close" data-trk-code-alt="prof-edu-project-open"><span class="">(Open)</span>1 project</dt><dd class="single"><ul><li class="row-first"><h6>Patient Information Management System</h6><p><span>to record the Patient Information Management System
+with Human Computer Interaction Easily.</span><a data-li-trk-code="prof-edu-project-more" href="#background-projects">View</a></p></li></ul></dd><dt data-trk-code="prof-edu-honor-close" data-trk-code-alt="prof-edu-honor-open"><span class="">(Open)</span>1 honor or award</dt><dd class="single"><ul><li class="row-first"><h6>Secondary Class</h6><p><span>GPA: 3.43</span><a data-li-trk-code="prof-edu-honor-more" href="#background-honors">View</a></p></li></ul></dd></dl></div></div></div><div id="education-45143993" class="editable-item section-item"><div id="education-45143993-view"><div class="education "><h4 class="summary fn org"><a href="/college/?eduSchool=La+Salle+Collage+Bangkok&amp;trk=prof-edu-school-name" title="Find other members who attended La Salle Collage Bangkok">La Salle Collage Bangkok</a><span class="edit-tools"><a href="javascript:void(0);" class="edit-section" data-trk="{&quot;componentKey&quot; : &quot;profile-edit-education-form&quot;, &quot;infoParams&quot; : { &quot;source&quot; : &quot;prof-edit-background-edit_education-link&quot;, &quot;educationID&quot; : &quot;45143993&quot;}}">Edit</a><span class="edit-order">Re-order</span></span></h4><h5><span class="degree">Student, </span><span class="major"><a href="/search?search=&amp;keywords=Mathematics+Science&amp;sortCriteria=R&amp;keepFacets=true&amp;trk=prof-edu-field_of_study" title="Find users with this keyword">Mathematics Science</a></span></h5><span class="education-date"><time datetime="1994">1994</time><time datetime="2007"> – 2007</time></span><p class="notes">was a student at La Salle Collage Bangkok (School)</p><dl class="education-associated"><dt data-trk-code="prof-edu-honor-close" data-trk-code-alt="prof-edu-honor-open"><span class="">(Open)</span>3 honors and awards</dt><dd class=""><ul><li class="row-first"><h6>Good Studying - Grade 11</h6><p><span>GPA: 3.5+</span><a data-li-trk-code="prof-edu-honor-more" href="#background-honors">View</a></p></li><li><h6>2nd Round Competition of Chulalongkorn University's Science Weekly Day</h6><p><span>2nd Round Competition of Chulalongkorn University's Science Weekly Day
+-- Subject: Computer
+
+PS. There are 5 subjests:
+1....</span><a data-li-trk-code="prof-edu-honor-more" href="#background-honors">View</a></p></li><li class="row-first"><h6>ABAC 's Science Weekly Day's Competition</h6><p><span>ABAC 's Science Weekly Day's Competition
+1st Round: 2th.
+3rd Round: 27th.</span><a data-li-trk-code="prof-edu-honor-more" href="#background-honors">View</a></p></li></ul></dd></dl></div></div></div><div id="education-141156382" class="editable-item section-item"><div id="education-141156382-view"><div class="education "><h4 class="summary fn org"><a href="/college/?eduSchool=CAT+%28Computer+Association+Thailand%29+%40Central+City+Bangna+%28Thailand%29&amp;trk=prof-edu-school-name" title="Find other members who attended CAT (Computer Association Thailand) @Central City Bangna (Thailand)">CAT (Computer Association Thailand) @Central City Bangna (Thailand)</a><span class="edit-tools"><a href="javascript:void(0);" class="edit-section" data-trk="{&quot;componentKey&quot; : &quot;profile-edit-education-form&quot;, &quot;infoParams&quot; : { &quot;source&quot; : &quot;prof-edit-background-edit_education-link&quot;, &quot;educationID&quot; : &quot;141156382&quot;}}">Edit</a><span class="edit-order">Re-order</span></span></h4><h5><span class="major"><a href="/search?search=&amp;keywords=C+Proramming&amp;sortCriteria=R&amp;keepFacets=true&amp;trk=prof-edu-field_of_study" title="Find users with this keyword">C Proramming</a></span></h5><span class="education-date"><time datetime="2002">2002</time><time datetime="2002"> – 2002</time></span><p class="notes">Studying C Programming</p></div></div></div><div id="education-141156067" class="editable-item section-item"><div id="education-141156067-view"><div class="education "><h4 class="summary fn org"><a href="/college/?eduSchool=Patana+Technic+School+%40Central+City+Bangna+%28Thailand%29&amp;trk=prof-edu-school-name" title="Find other members who attended Patana Technic School @Central City Bangna (Thailand)">Patana Technic School @Central City Bangna (Thailand)</a><span class="edit-tools"><a href="javascript:void(0);" class="edit-section" data-trk="{&quot;componentKey&quot; : &quot;profile-edit-education-form&quot;, &quot;infoParams&quot; : { &quot;source&quot; : &quot;prof-edit-background-edit_education-link&quot;, &quot;educationID&quot; : &quot;141156067&quot;}}">Edit</a><span class="edit-order">Re-order</span></span></h4><h5><span class="degree">System Engineering, </span><span class="major"><a href="/search?search=&amp;keywords=PC+Hardware+%26+Software+repairing+and+Complementary&amp;sortCriteria=R&amp;keepFacets=true&amp;trk=prof-edu-field_of_study" title="Find users with this keyword">PC Hardware &amp; Software repairing and Complementary</a>, </span><span>-</span></h5><span class="education-date"><time datetime="2001">2001</time><time datetime="2002"> – 2002</time></span><p class="notes">To repairing and complementary the Personal Computer..</p></div></div></div></div>
+	
+	
 </div>
 <div id="experiences">
 	<div class="myname">experiences</div>
@@ -20,44 +33,7 @@
 	<div class="myname">awards</div>
 </div>
 <div id="sn">
-	<div class="myname">sn</div>
-	<br>====================
-	<br>My Channels' Networking
-	<br>====================
-	<br>
-	<br><a target="_blank" href="mailto:nikom2532@hotmail.com">Windows Live Hotmail</a> : nikom2532@hotmail.com
-	<br><a target="_blank" href="https://www.ibm.com/developerworks/mydeveloperworks/profiles/user/ArmingHuang">IBM Developer works</a>
-	<br><a target="_blank" href="http://twitter.com/nikom2532">twitter: @nikom2532</a>
-	<br><a target="_blank" href="https://plus.google.com/115185082420374327964/">Google plus</a>
-	<br><a target="_blank" href="http://alturl.com/93nvt">Facebook</a>
-	<br><a target="_blank" href="http://wwww.weibo.com/nikom2532/">SINA Weibo(新浪微博)</a>
-	<br><a target="_blank" href="http://www.skype.com">Skype name</a> : nikom2532
-	<br><a target="_blank" href="http://www.whatsapp.com">Whatapp</a> : (+66)855065953
-	<br><a target="_blank" href="http://www.viber.com">Viber</a> : (+66)855065953
-	<br><a target="_blank" href="http://line.naver.jp/en/">Line (ライン)</a> : (+66)855065953
-	<br><a target="_blank" href="http://weixin.qq.com">WeiXin (微信)</a> : (+66)855065953
-	<br><a target="_blank" href="http://www.imqq.com/">Tencent QQ</a> : 2658406838
-	<br><a target="_blank" href="mailto:1005892981@qq.com">QQ 邮箱</a> : 2658406838@qq.com
-	<br><a target="_blank" href="mailto:nikom2532@yahool.com">Yahoo Mail</a>: nikom2532@yahoo.com
-	<br><a target="_blank" href="mailto:nikom2532@gmail.com">Gmail</a> : nikom2532@gmail.com
-	<br><a target="_blank" href="http://www.renren.com/profile.do?id=383146032">renren (人人网)</a>
-	<br><a target="_blank" href="http://soundcloud.com/nikom2532">SoundCloud</a>
-	<br><a target="_blank" href="http://t.qq.com/nikom2532">tweet.qq</a>
-	<br><a target="_blank" href="http://instagr.am">Instagram</a> : nikom2532
-	<br><a target="_blank" href="http://www.linkedin.com/in/nikom2532">LinkedIn</a>
-	<br><a target="_blank" href="http://www.hi5.com/friend/profile/displayProfile.do?userid=226392149">Hi5</a>
-	<br><a target="_blank" href="http://pulse.yahoo.com/nikom2532">YAHOO! PULSE</a>
-	<br><a target="_blank" href="http://www.flickr.com/photos/nikom2532">Flickr
-	<br></a><a target="_blank" href="http://www.youtube.com/nikom2532">Youtube Channel</a>
-	<br><a target="_blank" href="http://picplz.com/user/nikom2532/">Picplz</a>
-	<br><a target="_blank" href="http://friendfeed.com/nikom2532">friendfeed</a>
-	<br><a target="_blank" href="http://voxer.com">Voxer</a> : arming.hang.fb.1289836787
-	<br><a target="_blank" href="http://www.blogger.com/profile/02508875033021847292">Blogger</a>
-	<br><a target="_blank" href="https://foursquare.com">Foursquare</a> : nikom2532
-	<br><a target="_blank" href="http://www.spriiing.com">Spriiing</a> : 100BSS11 <font color="#ff0000">[not avalable right now]</font>
-	<br><a target="_blank" href="http://www.tango.me">Tango</a> : (+66)855065953 <font color="#ff0000">[not avalable right now]</font>
-	<br><a target="_blank" href="http://www.fring.com/">Fring</a> : (+66)855065953 <font color="#fc0500">[few times]</font>
-	<br><a target="_blank" href="https://www.facebook.com/channel.v.bangkok.music">Channel [V] Bangkok Music</a>
+	<?php include("./views/html/socialnetwork.php"); ?>
 </div>
 
 <?php
