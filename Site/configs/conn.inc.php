435986474ae14ecb8d16f780f115b3006e988762
farrutt <farrutt.th@gmail.com>
2025-03-28 11:18:48 +0700

    2025-03-28: lost file Config_conn.ini.php

--- Site/configs/conn.inc.php
+++
@@ -1,9 +0,0 @@
-<?
-	  //		('host','username','password','database')
-	  $db=new DB('localhost','iming','iming','iming');
-	  //$db->open();
-	  //$db->query("");
-	  //$result = $db->fetchArray();
-	  //$db->freeResult();
-	  //$db->close();
-?>
